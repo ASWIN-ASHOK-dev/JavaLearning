5262f74b6396e640c722e871dbe3fd6be373571a
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-07 00:02:42 +0530

    learnt input

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String user = new String();
+        String user = ""
         Scanner scanner = new Scanner(System.in);
         System.out.println("what is your name ");
         user  = scanner.next();
