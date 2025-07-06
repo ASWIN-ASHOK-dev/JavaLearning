bb870599b258af323638e2de6a04d53347f6c5bb
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-06 21:35:40 +0530

    First Commit

---
+++ src/com/ak/Main.java
@@ -0,0 +1,12 @@
+package com.ak;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+       final String owner = "Aswin Ashok ";
+
+        System.out.println(owner + 2/3);
+    }
+}
