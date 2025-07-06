4931ac3e3d64a829c502253c4ddc5bb51ca6097e
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-07 00:55:44 +0530

    mortage calculator v1

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("what is your name ");
-        String user  = scanner.next();
-        System.out.println("Welcome " + user);
+        System.out.println("Welcome to mortage calculator ");
+        System.out.print("Enter the principal amount :");
+        int Principal = scanner.nextInt();
+        System.out.print("Enter the rate :");
+        double monthlyInterestRate = (scanner.nextFloat()/100)/12;
+        System.out.print("Enter the time in years :");
+        int months = scanner.nextInt()*12;
+        Double mortage = Principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, months) / (Math.pow(1 + monthlyInterestRate, months) - 1);
+        System.out.println("Your Mortage is :- $" + String.format("%.2f", mortage));
+
     }
 }
