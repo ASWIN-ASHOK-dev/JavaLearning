9bc9913a57ad458810731032598d5eab15778416
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-07 00:03:46 +0530

    learnt input fixed

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -1,15 +1,11 @@
 package com.ak;
-
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String user = ""
         Scanner scanner = new Scanner(System.in);
         System.out.println("what is your name ");
-        user  = scanner.next();
-        System.out.println("Welcome "+user);
+        String user  = scanner.next();
+        System.out.println("Welcome " + user);
     }
 }
