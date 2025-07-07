af09c0f91d83ccd79b7b4e361ad2a4aa57e86aea
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-07 22:11:18 +0530

    FizzBuzz problem solution

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Just doing it,so I do not need to run it everytime :)
+        while (true)
+            fizzBuzz();
+    }
+    //FizzBuzz Problem
+    static void fizzBuzz() {
+        System.out.print("Enter a Number:-> ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to mortage calculator ");
-        System.out.print("Enter the principal amount :");
-        int Principal = scanner.nextInt();
-        System.out.print("Enter the rate :");
-        double monthlyInterestRate = (scanner.nextFloat()/100)/12;
-        System.out.print("Enter the time in years :");
-        int months = scanner.nextInt()*12;
-        Double mortage = Principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, months) / (Math.pow(1 + monthlyInterestRate, months) - 1);
-        System.out.println("Your Mortage is :- $" + String.format("%.2f", mortage));
-
+        int number = scanner.nextInt();
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
     }
 }
