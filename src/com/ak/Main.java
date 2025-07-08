8f8050a85f0e727c89a8905e0624bb63c4fe4ccd
Aswin Ashok-Linux <aswinashok3907@gmail.com>
2025-07-08 20:00:10 +0530

    My first OOP code

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -1,24 +1,28 @@
+//OOP learning
 package com.ak;
-import java.util.Scanner;
+class Student{
+    String name;
+    byte age;
 
-public class Main {
-    public static void main(String[] args) {
-        //Just doing it,so I do not need to run it everytime :)
-        while (true)
-            fizzBuzz();
+    Student(){
+        System.out.println("Object : "+ this +" Note:Using normal create is not a good practise,Consider Initialising it with name and age.");
     }
-    //FizzBuzz Problem
-    static void fizzBuzz() {
-        System.out.print("Enter a Number:-> ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        if (number % 5 == 0 && number % 3 == 0)
-            System.out.println("FizzBuzz");
-        else if (number % 3 == 0)
-            System.out.println("Buzz");
-        else if (number % 5 == 0)
-            System.out.println("Fizz");
-        else
-            System.out.println(number);
+    Student(String name,byte age){
+        this.name = name;
+        this.age = age;
+    }
+
+    String getInfo(){
+        return  name + " " + age;
+    }
+}
+public class Main{
+    public static void main(String[] arg) {
+        Student student1 = new Student();
+        student1.name = "Alan";
+        student1.age = 23;
+        System.out.println(student1.getInfo());
+        Student student2 = new Student("Alex",(byte)20);
+        System.out.println(student2.getInfo());
     }
 }
