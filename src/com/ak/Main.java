07118e205044a6e0a7b2b2bfecfb475b2a28e2ea
Aswin Ashok-Arch <aswinashok3907@gmail.com>
2025-07-11 21:53:58 +0530

    Finished OOP tutorial final one is static

--- src/com/ak/Main.java
+++ src/com/ak/Main.java
@@ -1,28 +1,12 @@
 //OOP learning
 package com.ak;
-class Student{
-    String name;
-    byte age;
-
-    Student(){
-        System.out.println("Object : "+ this +" Note:Using normal create is not a good practise,Consider Initialising it with name and age.");
-    }
-    Student(String name,byte age){
-        this.name = name;
-        this.age = age;
-    }
-
-    String getInfo(){
-        return  name + " " + age;
-    }
+class House{
+    int members;
+    static String name;
 }
 public class Main{
-    public static void main(String[] arg) {
-        Student student1 = new Student();
-        student1.name = "Alan";
-        student1.age = 23;
-        System.out.println(student1.getInfo());
-        Student student2 = new Student("Alex",(byte)20);
-        System.out.println(student2.getInfo());
+    public static void main(String[] args){
+        House.name="VS";
+        System.out.println(House.name);
     }
 }
